953cecbc68078e0660781bb245e5169d613f4257
skynet1881 <skynettech1992@gmail.com>
2025-01-26 21:17:58 +0100

    Wip

--- section-2/function.rs
+++ section-2/function.rs
@@ -1,29 +1,49 @@
 // Functions in Rust
 
-fn main() {
-    println!("Hello, world!");
-
-    // Function call
+fn main()
+{
+    // basics of functions
+    print_hello();
     another_function();
 
-    // Sum function
+    // sum of numbers
     sum(10, 20);
+    sum(-10, 10);
+    sum(0, 0);  
 
-    // bmi function
+    // subtraction of numbers
+    sub(10, 20);
+    sub(-10, 10);
+    sub(20, 10);
+
+    // bmi calculation
     let weight = 81.0;
     let height = 1.75;
     let bmi = bmi(weight, height);
     println!("BMI: {}", bmi);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn print_hello()
+{
+    println!("Hello, World!");
 }
 
-fn sum(a: i32, b: i32) {
+fn another_function()
+{
+    println!("Another function");
+}
+
+fn sum(a: i32, b: i32)
+{
     println!("Sum of {} and {} is {}", a, b, a + b);
 }
 
-fn bmi(weight: f32, height: f32) -> f32 {
+fn sub(a: i32, b: i32)
+{
+    println!("Subtraction of {} and {} is {}", a, b, a - b);
+}
+
+fn bmi(weight: f32, height: f32) -> f32
+{
     weight / (height * height)
 }
