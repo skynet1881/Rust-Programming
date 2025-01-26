a25d5eb67e0a3a4228154dcb261d5804cb817c42
skynet1881 <skynettech1992@gmail.com>
2025-01-26 20:07:40 +0100

    Wip

--- section-1/compound.rs
+++ section-1/compound.rs
@@ -1,38 +1,44 @@
 // Compound data types in Rust
-// Arrays, Tuples, Slices and strings
+// Arrays, Tuples, Slices, strings
 
-// Arrays
 fn main()
 {
+    // Arrays
     let arr = [1, 2, 3, 4, 5];
-    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    let numbers: [u32; 5] = [1, 2, 3, 4, 5];
+    let empty: [u32; 0] = [];
 
-    println!("Array: {:?}", arr);
+    println!("Array: {:?}", numbers);
 
-    let fruits = ["apple", "banana", "mango"];
-    println!("Fruits: {}", fruits[0]);
-    println!("Fruits: {}", fruits[1]);
-    println!("Fruits: {}", fruits[2]);
+    let fruits = ["apple", "banana", "cherry"];
+    println!("Fruits: {:?}", fruits);
+    println!("First fruit: {}", fruits[0]);
+    println!("Second fruit: {}", fruits[1]);
 
     // Tuples
     let insan1: (String, i32, bool) = ("Melis".to_string(), 25, true);
     let insan2: (&str, i32, bool) = ("Can", 32, false);
     println!("Name: {}", insan1.0);
     println!("Age: {}", insan1.1);
-    println!("Dogruluk: {}", insan1.2);
+    println!("Is it correct: {}", insan1.2);
 
     println!("Name: {}", insan2.0);
+    println!("Age: {}", insan2.1);
+    println!("Is it correct: {}", insan2.2);
 
-    // Slices: number slices
+    // Slices
     let arr = [1, 2, 3, 4, 5];
-    let slice = &arr[1..3];
+    let slice = &arr[1..4];
+    
     println!("Slice: {:?}", slice);
 
     // Strings
     let name = "Melis";
-    let mut ulke: String = "Turkey".to_string();
+    let mut country: String = "Turkey".to_string();
+    
     println!("Name: {}", name);
-    println!("Country: {}", ulke);
-    ulke.push_str(" is a beautiful country");
-    println!("Country: {}", ulke);
+    println!("Country: {}", country);
+
+    country.push_str(" is a beautiful country");
+    println!("Country: {}", country);
 }
