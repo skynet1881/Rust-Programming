77d9712442275af73e6d49070b82e7840014d121
skynet1881 <skynettech1992@gmail.com>
2025-02-05 22:54:56 +0100

    Functions 1

---
+++ section-2-3/functions1.rs
@@ -0,0 +1,23 @@
+// Introduction to Functions in Rust
+
+// Functions are defined using the fn keyword
+// They can take arguments, do operations and return values
+
+fn define_function()
+{
+    println!("This is a function");
+}
+
+fn main()
+{
+    println!("Hello, world!");
+    define_function();
+    hello_world();
+}
+
+fn hello_world()
+{
+    println!("Hello, world!!!!!");
+}
+
+
