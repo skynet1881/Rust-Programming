e90b8124b255d896dcf8b2e41666e2835e3b1426
skynet1881 <skynettech1992@gmail.com>
2025-02-05 22:21:20 +0100

    Wip

---
+++ section-1/ders1.rs
@@ -0,0 +1,13 @@
+// Lesson 1: Hello Rust Programming
+
+fn main()
+{
+    println!("Hello Rust Programming!");
+    println!("Hadi kodlayalim!");
+
+    println!("Merhaba \nDunya!");
+
+    // {} is a placeholder
+    println!("The answer is: {}", 6);
+    println!("{} is {} years old", "Can", 32);
+}
