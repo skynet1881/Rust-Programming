19f347dc5be7760d7f1afac8315c507deec28d08
skynet1881 <skynettech1992@gmail.com>
2025-02-08 16:07:52 +0100

    Wip errors

---
+++ section-14/error-handling-1.rs
@@ -0,0 +1,16 @@
+// Lesson 1: What is Error Handling?
+// Rust has two types of errors:
+// 1. Recoverable errors: These can be handled, like file not found. Rust uses `Result<T, E>` for these.
+// 2. Unrecoverable errors: These cause the program to stop immediately using `panic!()`.
+// unrecoverable turkcesi ne?
+fn main()
+{
+    // Example of unrecoverable error
+    let number = 0;
+    if number == 0
+    {
+        panic!("Something went wrong, stop execution");
+    }
+
+    println!("Set voltages to 5V");
+}
